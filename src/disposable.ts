e8a831d0bfd1ca96752bb6501105b5f185b9a995
Mehdi <mehdi.0.saadi@gmail.com>
2025-10-31 23:07:25 +0330

    feat (DisposableCollection): implement DisposableCollection class

--- src/disposable.ts
+++ src/disposable.ts
@@ -1,3 +1,25 @@
 export interface IDisposable {
   dispose: () => void
 }
+
+export class DisposableCollection implements IDisposable {
+  private _disposables: (() => void)[] = []
+  private _disposed = false
+
+  add(d: IDisposable | (() => void)) {
+    d = typeof d === 'function' ? d : d.dispose
+
+    if (this._disposed)
+      return d()
+
+    this._disposables.push(d)
+  }
+
+  dispose() {
+    if (this._disposed)
+      return
+    this._disposed = true
+    this._disposables.forEach(d => d())
+    this._disposables = []
+  }
+}
