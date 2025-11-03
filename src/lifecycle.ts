f9b349867aafc3daa98c52fe45fa8be18226f2e1
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-11-03 21:44:36 +0330

    chore (lifecycle): add console warnings before return

--- src/lifecycle.ts
+++ src/lifecycle.ts
@@ -9,15 +9,20 @@ export class DisposableStore implements IDisposable {
   add(d: IDisposable | (() => void)) {
     d = typeof d === 'function' ? d : d.dispose
 
-    if (this._disposed)
+    if (this._disposed) {
+      console.warn('Trying to add a disposable to a disposed store. This is probably a bug.')
       return d()
+    }
 
     this._disposables.add(d)
   }
 
   dispose() {
-    if (this._disposed)
+    if (this._disposed) {
+      console.warn('Trying to dispose a disposed store. This is probably a bug.')
       return
+    }
+
     this._disposables.forEach(d => d())
     this._disposables.clear()
     this._disposed = true
