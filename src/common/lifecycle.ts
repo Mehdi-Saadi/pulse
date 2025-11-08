4c133363b4362a4176fbdaeabd97ef532db38bfb
Mehdi Saadi <mehdi.0.saadi@gmail.com>
2025-11-08 13:41:39 +0330

    feat (lifecycle): implement isDisposable helper function

--- src/common/lifecycle.ts
+++ src/common/lifecycle.ts
@@ -11,6 +11,13 @@ export interface IDisposable {
   dispose: () => void
 }
 
+/**
+ * Check if `thing` is {@link IDisposable disposable}.
+ */
+export function isDisposable<E>(thing: E): thing is E & IDisposable {
+  return typeof thing === 'object' && thing !== null && typeof (<IDisposable><any>thing).dispose === 'function' && (<IDisposable><any>thing).dispose.length === 0
+}
+
 export class DisposableStore implements IDisposable {
   private readonly _disposables: Set<IDisposable> = new Set()
   private _disposed = false
